a19959e0f9592ca997ca865f6d503d3aa0e85d9d
alvaro.ramiro <alraled@hotmail.es>
2016-04-26 14:12:23 +0000

    Laboratorio8

---
+++ Laboratorio8/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="f8dd89ac-ee0d-4f8d-8b42-e2cbf9ab4f10" description="SharePoint Add-in Feature" featureId="f8dd89ac-ee0d-4f8d-8b42-e2cbf9ab4f10" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Laboratorio8 Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="35ec9216-1ab9-4c61-b001-b0d8356007bf" />
+    <projectItemReference itemId="22b00ff3-8691-41cc-8db4-cff45454b6ba" />
+    <projectItemReference itemId="baf15651-79ba-44ac-92ff-31b7026ecb13" />
+    <projectItemReference itemId="a9426198-5f8c-4697-9ab6-bb33970869fb" />
+  </projectItems>
+</feature>
---
+++ Laboratorio8/Scripts/App.js
@@ -0,0 +1,91 @@
+﻿'use strict';
+
+function getQueryStringParams(sParam) {
+    var sPageUrl = window.location.search.substring(1);
+    var sUrlVariables = sPageUrl.split('&');
+    for (var i = 0; i < sUrlVariables.length; i++) {
+        var sParameterName = sUrlVariables[i].split('=');
+        if (sParameterName[0]== sParam) {
+            return sParameterName[1];
+        }
+    }
+}
+
+var currentContext;
+var hostWebContext;
+var web;
+var list;
+
+function getList(id) {
+    clearInfo();
+    var lista = hostWebContext.get_web().get_lists().getByTitle("PedidosClientes");
+    // var idCliente = $("#idCliente").val();
+
+    var query = new SP.CamlQuery();
+    query.set_viewXml("<ViewScope='RecursiveAll'><Query><Where><Contains><FieldRef Name='Cliente_x003a_ID'/><Value Type='Text'>" + id + "</Value></Contains></Where></Query></View>");
+    list = lista.getItems(query);
+
+    //TODO: Task 2. Submit query results
+    currentContext.load(list);
+    currentContext.executeQueryAsync(Function.createDelegate(this, onSuccess), Function.createDelegate(this, onFail));
+}
+
+function onSuccess() {
+    if (list.get_count() !=0) {
+        var listEnum = list.getEnumerator();
+        var n = 1;
+        var total = 0;
+        var tabla = $("#TablaPedidos tbody");
+        while (listEnum.moveNext()) {
+            var html = "<tr>";
+            var actual = listEnum.get_current();
+            var d = actual.get_item("Fecha");
+            var day = d.getDate();
+            var month = d.getMonth()+1;
+            var year = d.getFullYear();
+
+            html += "<td>" + actual.get_item("Numero_x0020_pedido") + "</td>";
+            html += "<td>" + day + "-" + month + "-" + year + "</td>";
+            html += "<td>" + actual.get_item("Total") + "€</td>";
+            n++;
+            tabla.append(html);
+        }
+
+        var nombre = actual.get_item("Cliente_x003a_Nombre_x0020_compl").get_lookupValue();
+        $("#Nombre").text(nombre);
+        $("#NumeroPedidos").text("Pedidos: "+(n+1));
+        $("#MostrarPedidos").css("display", "block");
+        $("#SinPedidos").css("display","none");
+    }
+
+        else {
+        $("#MostrarPedidos").css("display", "none");
+        $("#SinPedidos").css("display","block");
+    }
+
+}
+
+function onFail() {
+    alert("Error");
+}
+
+function clearInfo() {
+    var t = $("#TablaPedidos>tbody>tr");
+    t.remove();
+    $("#NumeroPedidos").text("");
+    $("#Total").text("");
+}
+
+function init() {
+    var hostUrl = decodeURIComponent(getQueryStringParams("SPHostUrl"));
+    currentContext = new SP.ClientContext.get_current();
+    hostWebContext = new SP.AppContextSite(currentContext, hostUrl);
+    web = hostWebContext.get_web();
+
+    var id = getQueryStringParams("SPListItemId");
+    getList(id);
+}
+
+$(document).ready(function() {
+    ExecuteOrDelayUntilScriptLoaded(init, "sp.js");
+});
